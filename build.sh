a60e364d88575f69bf4a01d9ea466639cee1b9b5
Trass3r <trass3r@gmail.com>
2021-08-27 10:18:59 +0200

    try

--- build.sh
+++ build.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 set -eux
 
-export CC="ccache gcc" CXX="ccache g++"
+#export CC="ccache gcc" CXX="ccache g++"
 export CROSSCC="ccache x86_64-w64-mingw32-gcc";
 export CROSSCXX="ccache x86_64-w64-mingw32-g++";
 export CFLAGS="-O3"
 export CXXFLAGS="${CFLAGS}"
-export LDFLAGS="-Wl,-O1,--sort-common,--as-needed -static-libgcc"
+export LDFLAGS="-static-libgcc"
 export CROSSCFLAGS="${CFLAGS} -fno-omit-frame-pointer -g -DWINE_NOWINSOCK -DUSE_WIN32_OPENGL -DUSE_WIN32_VULKAN"
-export CROSSLDFLAGS="${LDFLAGS}"
+export CROSSLDFLAGS="${LDFLAGS} -L`pwd`"
 
 rm -rf wine-tools wine-win64 wine-src wine-staging
 mkdir wine-tools wine-win64
@@ -32,6 +32,9 @@ cd wine-tools
 ../wine-src/configure --without-x --enable-win64
 make -j4 __tooldeps__
 cd ../wine-win64
+apt install -y wget unzip
+wget -q https://sdk.lunarg.com/sdk/download/latest/windows/vulkan-runtime-components.zip
+unzip -j vulkan-runtime-components.zip *x64/vulkan-1.dll
 ../wine-src/configure --without-x --enable-win64 --without-freetype --without-vkd3d --host=x86_64-w64-mingw32 --with-wine-tools=../wine-tools/
 make -j4 dlls/ddraw/all dlls/ddrawex/all dlls/wined3d/all
 make -j4 -k $(echo dlls/ddraw* dlls/d3d* dlls/dxgi dlls/wined3d/all | sed 's# #/all #g') || true
