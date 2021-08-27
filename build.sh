0f593edc889da5504229f40ee7eca19d9e2abd70
Trass3r <trass3r@gmail.com>
2021-08-27 08:59:09 +0200

    use ccache

--- build.sh
+++ build.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 set -eux
 
+export CC="ccache gcc" CXX="ccache g++"
+export CROSSCC="ccache x86_64-w64-mingw32-gcc";
+export CROSSCXX="ccache x86_64-w64-mingw32-g++";
+export CFLAGS="-O3 -fno-omit-frame-pointer -g -DWINE_NOWINSOCK -DUSE_WIN32_OPENGL -DUSE_WIN32_VULKAN"
+export CXXFLAGS="${CFLAGS}"
+export LDFLAGS="-Wl,-O1,--sort-common,--as-needed -static-libgcc"
+export CROSSCFLAGS="${CFLAGS}"
+export CROSSLDFLAGS="${LDFLAGS}"
+
 rm -rf wine-tools wine-win64 wine-src wine-staging
 mkdir wine-tools wine-win64
 
@@ -22,7 +31,7 @@ cd wine-tools
 ../wine-src/configure --without-x --enable-win64
 make -j4 __tooldeps__
 cd ../wine-win64
-../wine-src/configure --without-x --enable-win64 --without-freetype --without-vkd3d --host=x86_64-w64-mingw32 CFLAGS="-O3 -fno-omit-frame-pointer -g -DWINE_NOWINSOCK -DUSE_WIN32_OPENGL -DUSE_WIN32_VULKAN" --with-wine-tools=../wine-tools/ LDFLAGS=" -static-libgcc"
+../wine-src/configure --without-x --enable-win64 --without-freetype --without-vkd3d --host=x86_64-w64-mingw32 --with-wine-tools=../wine-tools/
 make -j4 dlls/ddraw/all dlls/ddrawex/all dlls/wined3d/all
 make -j4 -k $(echo dlls/ddraw* dlls/d3d* dlls/dxgi dlls/wined3d/all | sed 's# #/all #g') || true
 mkdir -p ../$outdir
