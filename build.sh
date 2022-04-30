fd37086e77124e60f9bdb8e696856704ab2ab37a
Trass3r <trass3r@gmail.com>
2022-04-30 14:30:57 +0200

    remove separate tools

--- build.sh
+++ build.sh
@@ -35,14 +35,11 @@ fi
 fi
 
 ccache -z
-cd wine-tools
-../wine-src/configure --without-x --enable-win64
-make -j4 __tooldeps__
-cd ../wine-win64
+cd wine-win64
 apt install -y wget unzip
 wget -q https://sdk.lunarg.com/sdk/download/latest/windows/vulkan-runtime-components.zip
 unzip -j vulkan-runtime-components.zip *x64/vulkan-1.dll
-../wine-src/configure --without-x --enable-win64 --without-freetype --without-vkd3d --host=x86_64-w64-mingw32 --with-wine-tools=../wine-tools/
+../wine-src/configure --without-x --enable-win64 --without-freetype --without-vkd3d --host=x86_64-w64-mingw32
 make -j4 dlls/ddraw/all dlls/ddrawex/all dlls/wined3d/all
 make -j4 -k $(echo dlls/gdi32 dlls/ddraw* dlls/d3d* dlls/dxgi dlls/wined3d/all | sed 's# #/all #g') || true
 ccache -s
@@ -50,7 +47,7 @@ mkdir -p ../$outdir/64
 cp -v dlls/*/*.dll ../$outdir/64/
 
 cd ../wine-win32
-../wine-src/configure --without-x --without-freetype --without-vkd3d --host=i686-w64-mingw32 --with-wine-tools=../wine-tools/ --with-wine64=../wine-win64/
+../wine-src/configure --without-x --without-freetype --without-vkd3d --host=i686-w64-mingw32 --with-wine64=../wine-win64/
 make -j4 dlls/ddraw/all dlls/ddrawex/all dlls/wined3d/all
 make -j4 -k $(echo dlls/gdi32 dlls/ddraw* dlls/d3d* dlls/dxgi dlls/wined3d/all | sed 's# #/all #g') || true
 mkdir -p ../$outdir/32
