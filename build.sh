96a6064853109979438126cf1a7048b311aa9026
Trass3r <trass3r@gmail.com>
2023-02-27 08:05:01 +0100

    fix

--- build.sh
+++ build.sh
@@ -37,6 +37,7 @@ mkdir -p build && cd build
 ../wine-src/configure --enable-win64 --disable-tests --with-mingw --enable-archs=i386,x86_64
 make -j4 -k $(echo dlls/ddraw* dlls/d3d? dlls/dxgi dlls/wined3d/all | sed 's# #/all #g')
 ccache -svx
+mkdir -p ../$outdir
 cp -v **/*.{dll,pdb} ../$outdir/
 exit 0
 
